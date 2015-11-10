421abc808c5602a5c054851cf302157008537c66
AnithaPal <anitha.aravindvel@gmail.com>
2015-11-10 06:44:35 -0500

    play pause button toggling on playbar based on the song status feature added

--- scripts/album.js
+++ scripts/album.js
@@ -192,6 +192,22 @@ var updatePlayerBarSong = function() {
     $('.main-controls .play-pause').html(playerBarPauseButton);
 };
 
+
+var togglePlayFromPlayerBar = function(){
+    var $currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
+    if(currentSoundFile.isPaused()){
+        $currentlyPlayingCell.html(pauseButtonTemplate);
+        $(this).html(playerBarPauseButton);
+        currentSoundFile.play();
+    }else if(currentSoundFile){
+        $currentlyPlayingCell.html(playButtonTemplate);
+        $(this).html(playerBarPlayButton);
+        currentSoundFile.pause();
+        
+    }
+    
+}
+
 var playButtonTemplate = '<a class="album-song-button"> <span class= "ion-play"></span> </a>';
 var pauseButtonTemplate = '<a class="album-song-button"> <span class= "ion-pause"></span> </a>';
 var playerBarPlayButton = '<span class="ion-play"></span>';
@@ -205,6 +221,7 @@ var currentVolume = 80;
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
+var $playPauseButton = $('.main-controls .play-pause');
 
 
 
@@ -212,4 +229,5 @@ $(document).ready(function(){
     setCurrentAlbum(albumPicasso);
     $previousButton.click(previousSong);
     $nextButton.click(nextSong);
+    $playPauseButton.click(togglePlayFromPlayerBar);
 });
